24da13ce879ba10721548843c8687a658b311bb1
Your Name <you@example.com>
2018-03-14 19:33:35 +0900

    lag2

--- protos/lstm2.py
+++ protos/lstm2.py
@@ -3,22 +3,24 @@ import numpy as np
 import keras
 from keras.models import Model
 from keras.layers import Input, Dense, LSTM, Embedding, concatenate, BatchNormalization, Lambda, Activation, GRU, SimpleRNN, RNN
-from keras.layers import CuDNNGRU, CuDNNLSTM, StackedRNNCells, MaxPooling1D, AvgPool1D
+from keras.layers import CuDNNGRU, CuDNNLSTM, StackedRNNCells, MaxPooling1D, AvgPool1D, LeakyReLU
 from keras.layers.wrappers import TimeDistributed
 from keras import backend as K
 import tensorflow as tf
 
 MAX_SEQUENCE_LENGTH = 5
 
-LIST_COL = ['app', 'device', 'os', 'channel', 'hour',
-            'avg_app', 'avg_device', 'avg_os', 'avg_channel', 'avg_day', 'avg_hour']
+LIST_COL = ['app', 'device', 'os', 'channel', 'hour',  # 'sum_attr', 'last_attr',
+            #'ip',
+            'avg_ip', 'avg_app', 'avg_device', 'avg_os', 'avg_channel', 'avg_day', 'avg_hour', 'avg_ipdayhour']
+
 LIST_CONV_COL = ['avg_avg', 'max_avg', 'min_avg']
 
 LIST_ALL_COL = LIST_COL + LIST_CONV_COL + ['is_attributed']
 
 LIST_CAT_COL = ['app', 'device', 'os', 'channel', 'hour']
 
-LIST_FLOAT_COL = ['avg_app', 'avg_device', 'avg_os', 'avg_channel', 'avg_day', 'avg_hour']
+LIST_FLOAT_COL = ['avg_ip', 'avg_app', 'avg_device', 'avg_os', 'avg_channel', 'avg_day', 'avg_hour', 'avg_ipdayhour']
 
 LIST_DATA_COL = LIST_CAT_COL + LIST_FLOAT_COL
 
@@ -58,16 +60,20 @@ def get_lstm_sin(first_dences=[64, 64, 16],
                 for col in LIST_CAT_COL]
     avgs = concatenate([Lambda(K.expand_dims)(inputs[col]) for col in LIST_CONV_COL])
 
+    others = concatenate([Lambda(K.expand_dims)(inputs[col])
+                          for col in LIST_ALL_COL if col not in LIST_FLOAT_COL + LIST_CAT_COL], axis=2)
+
     floats = concatenate([Lambda(K.expand_dims)(inputs[col]) for col in LIST_FLOAT_COL], axis=2)
     old_target = Lambda(K.expand_dims)(inputs['is_attributed'])
 
-    out = concatenate(one_hots + [floats] + [old_target], axis=2)
+    out = concatenate(one_hots + [others] + [floats] + [old_target], axis=2)
 
     for i, size in enumerate(first_dences):
         out = Dense(size, name=f'first_dence_{i}_{size}')(out)
         if is_first_bn:
             out = BatchNormalization(name=f'first_bn_{i}_{size}')(out)
-        out = Activation('relu')(out)
+        out = LeakyReLU()(out)  #
+        #out = Activation('relu')(out)
 
     out = concatenate([out, avgs], axis=2)
 
@@ -76,14 +82,15 @@ def get_lstm_sin(first_dences=[64, 64, 16],
                 name='lstm')(out)
 
     gru = CuDNNGRU(16, name='gru')(out)
-    rnn = SimpleRNN(16, name='rnn')(out)
+    #rnn = SimpleRNN(lstm_size, name='rnn')(out)
 
-    merged = concatenate([lstm, gru, rnn])
+    merged = concatenate([lstm, gru])
 
     for i, size in enumerate(last_dences):
         merged = Dense(size, name=f'last_de1_{i}_{size}')(merged)
         if is_last_bn:
             merged = BatchNormalization(name=f'last_bn1_{i}_{size}')(merged)
+        #merged = LeakyReLU()(merged)  #
         merged = Activation('relu')(merged)
 
     preds = Dense(1, activation='sigmoid', name='last1')(merged)
