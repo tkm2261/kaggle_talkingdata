4c0d28162522308baadaa7e76132b3d58b5aed7b
Your Name <you@example.com>
2018-03-17 04:48:24 +0900

    lstm09450

--- protos/train_lag.py
+++ protos/train_lag.py
@@ -12,6 +12,7 @@ import tensorflow as tf
 from keras.callbacks import Callback, EarlyStopping, ReduceLROnPlateau, ModelCheckpoint, TensorBoard
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import StandardScaler
 from lstm2 import get_lstm_sin, LIST_ALL_COL, LIST_COL, LIST_DATA_COL, LIST_FLOAT_COL, LIST_CAT_COL, LIST_COL, LIST_CONV_COL
 import dask.dataframe as ddf
 import dask.multiprocessing
@@ -24,33 +25,6 @@ np.random.seed(0)
 from logging import getLogger
 
 logger = getLogger(None)
-
-DTYPE = None
-_DTYPE = {'click_id': np.float32,
-          'ip': np.float32,
-          'app': np.float32,
-          'device': np.float32,
-          'os': np.float32,
-          'channel': np.float32,
-          'click_time': str,
-          'attributed_time': str,
-          'is_attributed': np.float32,
-          'timediff': np.float32,
-          'year': np.uint16,
-          'month': np.uint8,
-          'day': np.uint8,
-          'dayofweek': np.uint16,
-          'hour': np.uint16,
-          'avg_ip': np.uint16,
-          'sum_attr': np.uint16,
-          'last_attr': np.float32,
-          'avg_app': np.float32,
-          'avg_device': np.float32,
-          'avg_os': np.float32,
-          'avg_channel': np.float32,
-          'avg_day': np.float32,
-          'avg_hour': np.float32
-          }
 
 
 try:
@@ -71,6 +45,7 @@ except IndexError:
 
 
 import gzip
+from sklearn.exceptions import NotFittedError
 
 
 def calc_batch_num(paths):
@@ -81,7 +56,7 @@ def calc_batch_num(paths):
     return cnt
 
 
-def data_generator(paths, repeat=True):
+def data_generator(paths, sc, repeat=True, is_train=True):
     while True:
         for path in paths:
             logger.debug(path)
@@ -89,19 +64,34 @@ def data_generator(paths, repeat=True):
             if os.path.exists(sv):
                 df = pd.read_pickle(sv)
             else:
-                df = pd.read_csv(path, dtype=DTYPE)
+                df = pd.read_csv(path)
                 # df.to_pickle(sv, protocol=-1)
             df = df.take(np.random.permutation(len(df))).reset_index(drop=True).fillna(-1)
+
             for start in range(0, df.shape[0], batch_size):
                 end = min(start + batch_size, df.shape[0])
-                data = df.iloc[start:end]
-
+                data = df.iloc[start:end].copy()
+                """
+                if is_train:
+                    sc.partial_fit(data.values)
+                    with open('sc.pkl', 'wb') as f:
+                        pickle.dump(sc, f, -1)
+                try:
+                    _data = sc.transform(data.values)
+                    for i, col in enumerate(data):
+                        data[col] = _data[:, i]
+                except NotFittedError:
+                    logger.warn('sc is not fitted. passing')
+                    pass
+                """
                 targets = np.array(data['is_attributed'].values)
 
                 inputs = []
                 for col in LIST_COL:
                     if col in ['sum_attr', 'last_attr', 'ip']:
                         cols = [col for i in range(5)]
+                    elif col == 'click_diff':
+                        cols = [f'{col}_{i}' for i in range(1, 6)[::1]]
                     else:
                         cols = [f'{col}_{i}' for i in range(1, 5)[::-1]] + [col]
                     inputs.append(data[cols].values)
@@ -134,9 +124,10 @@ class LoggingCallback(Callback):
 
     def __init__(self, valid_path):
         self.valid_path = valid_path
-        self.valid_data = list(data_generator(self.valid_path, repeat=False))
+        #self.valid_data = list(data_generator(self.valid_path, repeat=False))
 
     def on_epoch_end(self, epoch, logs={}):
+        """
         labels = []
         preds = []
         for x_batch, y_batch in self.valid_data:
@@ -148,7 +139,6 @@ class LoggingCallback(Callback):
         msg = "Epoch: %i, %s" % (epoch, ", ".join("%s: %f" % (k, logs[k]) for k in sorted(logs))) + f', auc: {auc}'
         """
         msg = "Epoch: %i, %s" % (epoch, ", ".join("%s: %f" % (k, logs[k]) for k in sorted(logs)))
-        """
 
         logger.info(msg)
 
@@ -172,11 +162,11 @@ def main():
     logger.addHandler(handler)
 
     logger.info(f'file: {param_file}, params: {model_params}')
-    paths = sorted(glob.glob('../data/dmt_train_lag2/*.csv.gz'))
-    train_path, valid_path = train_test_split(paths, test_size=0.05, random_state=42)
+    #paths = sorted(glob.glob('../data/dmt_train_lag2/*.csv.gz'))
+    #train_path, valid_path = train_test_split(paths, test_size=0.05, random_state=42)
 
-    # train_path = paths[:-2]
-    # valid_path = paths[-2:]
+    train_path = sorted(glob.glob('../data/dmt_train_lstm/*.csv.gz'))
+    valid_path = sorted(glob.glob('../data/dmt_valid_lstm/*.csv.gz'))
     # steps_per_epoch: 1784, validation_steps: 74
     steps_per_epoch = 148  # calc_batch_num(train_path)
     validation_steps = 148  # calc_batch_num(valid_path)
@@ -204,16 +194,17 @@ def main():
 
     logger.info(f'steps_per_epoch: {steps_per_epoch}, validation_steps: {validation_steps}')
 
+    sc = StandardScaler()
     model = get_lstm_sin(**model_params)
-    model.load_weights(filepath='weights/best_weights.hdf5')
+    # model.load_weights(filepath='weights/best_weights.hdf5')
 
     epochs = 10000
-    model.fit_generator(generator=data_generator(train_path),
+    model.fit_generator(generator=data_generator(train_path, sc, is_train=True),
                         steps_per_epoch=steps_per_epoch,
                         epochs=epochs,
                         verbose=2,
                         callbacks=callbacks,
-                        validation_data=data_generator(valid_path),
+                        validation_data=data_generator(valid_path, sc, is_train=False),
                         validation_steps=validation_steps
                         )
 
